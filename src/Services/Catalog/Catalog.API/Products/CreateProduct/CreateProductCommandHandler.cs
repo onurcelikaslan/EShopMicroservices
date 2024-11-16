d47ff88dfc7fa7cc54c4c2d6df644e0f29cda69d
Onur Celikaslan <onurcelikaslan@outlook.com>
2024-11-16 12:57:15 +0000

    Added Update, Delete, GetById and GetByCategory handlers and endpoints

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,5 +1,5 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
